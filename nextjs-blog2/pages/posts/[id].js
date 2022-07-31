c70a97d79a8360c8e15a0d99dfe8937bf2ef8146
Tomasz Marszalek <marstomek@gmail.com>
2022-07-31 13:13:06 +0200

    Posts from file!

--- nextjs-blog2/pages/posts/[id].js
+++ nextjs-blog2/pages/posts/[id].js
@@ -1,19 +1,34 @@
 import Layout from "../../components/layout"
-import { getAllPostIds } from '../../lib/posts'
+import { getAllPostIds, getPostData } from '../../lib/posts'
 
-const Post = () => {
-    return <Layout>...</Layout>
+const Post = ({postData}) => {
+    return (
+        <Layout>
+        {postData.title}
+        <br />
+        {postData.id}
+        <br />
+        {postData.date}
+      </Layout>
+    )
 
 }
 
-// export async function getStaticPaths(){
-//     console.log('paths...')
-//     const paths = getAllPostIds();
-//     return {paths, fallback: false}
-// }
+export async function getStaticPaths(){
+    console.log('paths...')
+    const paths = getAllPostIds();
+    return {paths, fallback: false}
+}
 
-// export async function getStaticProps({ params }) {
-//     // Fetch necessary data for the blog post using params.id
-//   }
+export async function getStaticProps({ params }) {
+    // Fetch necessary data for the blog post using params.id
+    const postData = getPostData(params.id);
+
+    return {
+        props: {
+            postData
+        }
+    }
+  }
 
 export default Post;
