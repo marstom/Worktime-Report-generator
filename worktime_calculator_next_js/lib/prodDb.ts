85560ed2788a90494a69829ed58bef01f449d46e
Tomasz Marszalek <marstomek@gmail.com>
2023-01-06 10:11:19 +0100

    Database initialize async, use dependency injection

--- worktime_calculator_next_js/lib/prodDb.ts
+++ worktime_calculator_next_js/lib/prodDb.ts
@@ -1,26 +1,26 @@
 import { JSONDatabase } from "./database";
 import { loadedSettings } from "./settings";
 
-// TODO read settings
-let settings: { [key: string]: string } = {};
+const initializeDatabase = async () => {
+  let settings: { [key: string]: string } = {};
+  let db: JSONDatabase;
+  try {
+    console.log("loading settings....");
+    settings = await loadedSettings();
+    console.log("Loaded settings!");
+  } catch (error) {
+    console.log("NOT loaded settings");
+    console.error("Cannot load settings in given time...");
+  }
 
-let db: JSONDatabase;
-try {
-  console.log("loading settings....");
-  settings = await loadedSettings();
-  console.log("Loaded settings!");
-} catch (error) {
-  console.log("NOT loaded settings");
-  console.error("Cannot load settings in given time...");
-}
+  if (settings["activeDatabaseName"]) {
+    db = new JSONDatabase(settings["activeDatabaseName"]);
+  } else {
+    throw new Error(
+      'Please configure path to database in settings.json i.e. "activeDatabaseName": "./lib/db/FROM_settigs.json"',
+    );
+  }
+  return db;
+};
 
-if (settings["activeDatabaseName"]) {
-  // db = new JSONDatabase(process.env.JSON_DB);
-  db = new JSONDatabase(settings["activeDatabaseName"]);
-} else {
-  throw new Error(
-    "Please configure path to database i.e JSON_DB=./lib/db/timeEntrysDb.json",
-  );
-}
-
-export default db;
+export default initializeDatabase;
--- worktime_calculator_next_js/pages/api/worktimesheet/monthly/[year].ts
+++ worktime_calculator_next_js/pages/api/worktimesheet/monthly/[year].ts
@@ -3,15 +3,17 @@ import { sessionOptions } from "lib/session";
 import { withIronSessionApiRoute } from "iron-session/next";
 import { unauthorized } from "lib/auth";
 import { NextApiRequest, NextApiResponse } from "next";
+import initializeDatabase from "lib/prodDb";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const db = await initializeDatabase();
   // @ts-ignore
   if (unauthorized(req.session.user, res)) {
     return;
   }
   const query = req.query;
   // @ts-ignore
-  res.status(200).json(await getTimeEntrysForDate(query.year));
+  res.status(200).json(db, await getTimeEntrysForDate(query.year));
 }
 
 export default withIronSessionApiRoute(handler, sessionOptions);
